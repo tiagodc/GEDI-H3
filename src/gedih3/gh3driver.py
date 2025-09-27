e7a25bd9516c31075c2890b371cca59ad3322852
Tiago De Conto <decontot@gsapp22.gshpc.umd.edu>
2025-09-27 19:59:34 -0400

    grab index name directly

--- src/gedih3/gh3driver.py
+++ src/gedih3/gh3driver.py
@@ -15,7 +15,7 @@ def gh3_list_parts(gh3_root_dir=GH3_DEFAULT_H3_DIR, product=None):
     h3_ids = list({os.path.basename(i).replace('.parquet', '') for i in files})
     return h3_ids    
 
-def gh3_load_part(h3_part_id, h3_product_vars, query=None, h3_res_level=12, gh3_dir=GH3_DEFAULT_H3_DIR):
+def gh3_load_part(h3_part_id, h3_product_vars, query=None, gh3_dir=GH3_DEFAULT_H3_DIR):
     df = None
     for prod,cols in h3_product_vars.items():
         if 'shot_number' not in cols:
@@ -23,20 +23,21 @@ def gh3_load_part(h3_part_id, h3_product_vars, query=None, h3_res_level=12, gh3_
         ipath = os.path.join(gh3_dir, prod.lower(), f"{h3_part_id}.parquet")
         idf = pd.read_parquet(ipath, engine='pyarrow', columns=cols)
         if df is None:
+            idx_name = idf.index.name
             df = idf.reset_index()
         else:
             df = df.merge(idf, how='inner', on='shot_number', suffixes=(f'', f'_{prod}'))
-    df = df.set_index(f'h3_{h3_res_level:02d}')
+    df = df.set_index(idx_name)
     
     if query is not None:
         df = df.query(query)
     
     return df
 
-def gh3_load_all(h3_product_vars, region=None, query=None, h3_res_level=12, gh3_dir=GH3_DEFAULT_H3_DIR): 
+def gh3_load_all(h3_product_vars, region=None, query=None, gh3_dir=GH3_DEFAULT_H3_DIR): 
     h3_ids = gh3_list_parts(gh3_root_dir=gh3_dir)
     
     if region is not None:
         h3_ids = intersect_h3_geometries(region, h3_ids=h3_ids)
         
-    return dask.dataframe.from_map(gh3_load_part, h3_ids, h3_product_vars=h3_product_vars, query=query, h3_res_level=h3_res_level, gh3_dir=gh3_dir)
+    return dask.dataframe.from_map(gh3_load_part, h3_ids, h3_product_vars=h3_product_vars, query=query, gh3_dir=gh3_dir)
