a771cf1e76f59656c43f6baa50cfe51d0d3e5ad4
Tiago De Conto <decontot@gsapp22.gshpc.umd.edu>
2025-10-30 14:35:11 -0400

    create tmp dir before sql con

--- src/gedih3/sqlutils.py
+++ src/gedih3/sqlutils.py
@@ -1,14 +1,15 @@
-import os, psutil, duckdb
+import os,duckdb
 
 from .config import GH3_DEFAULT_H3_DIR, GH3_DEFAULT_TMP_DIR
 from .utils import get_system_resources
 
-def init_duckdb(threads=None, memory_limit=None, temp_directory=None, max_temp_size=None):
-    temp_directory = temp_directory if temp_directory is not None else f"{GH3_DEFAULT_TMP_DIR}/duckdb"
-    
-    cpus, ram, storage = get_system_resources(disk_path=temp_directory)
+def init_duckdb(threads=None, memory_limit=None, tmp_directory=None, max_tmp_size=None):
+    tmp_directory = tmp_directory if tmp_directory is not None else f"{GH3_DEFAULT_TMP_DIR}/duckdb"
+    os.makedirs(tmp_directory, exist_ok=True)
+
+    cpus, ram, storage = get_system_resources(disk_path=tmp_directory)
     memory_limit = memory_limit if memory_limit is not None else int(ram * 0.75)
-    max_temp_size = max_temp_size if max_temp_size is not None else storage // 4
+    max_tmp_size = max_tmp_size if max_tmp_size is not None else storage // 4
     threads = threads if threads is not None else max(1, cpus // 4)
 
     con = duckdb.connect()
@@ -17,7 +18,7 @@ def init_duckdb(threads=None, memory_limit=None, temp_directory=None, max_temp_s
     con.execute("SET enable_progress_bar = true;")
     con.execute("SET preserve_insertion_order = false;")
     con.execute(f"SET memory_limit='{memory_limit}GB';")
-    con.execute(f"SET temp_directory='{temp_directory}';")
-    con.execute(f"SET max_temp_directory_size='{max_temp_size}GB';")
+    con.execute(f"SET tmp_directory='{tmp_directory}';")
+    con.execute(f"SET max_tmp_directory_size='{max_tmp_size}GB';")
     con.execute(f"PRAGMA threads={threads};")
     return con
