f1c99de7d0cf9585dae5d6e0375daff68e159d17
Tiago De Conto <decontot@gsapp22.gshpc.umd.edu>
2026-05-14 12:47:11 -0400

    bump version to 0.10.0

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ import sys
 project = "gedih3"
 copyright = "2026, University of Maryland. All Rights Reserved"
 author = "Tiago de Conto and Amelia Grace Holcomb"
-release = "0.9.6"
+release = "0.10.0"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
